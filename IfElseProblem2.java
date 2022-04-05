03435384b166baad5d6daa1adb73d7e8654cb842
mayur505x <102165605+mayur505x@users.noreply.github.com>
2022-04-05 23:40:02 +0530

    Update IfElseProblem2.java

--- IfElseProblem2.java
+++ IfElseProblem2.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Day2Problem2 {
+public class IfElseProblem2 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter number: ");
